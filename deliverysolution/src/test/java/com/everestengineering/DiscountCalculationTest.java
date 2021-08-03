de1089fd96bddfe942d1709320059b702c6575a7
Guru-kn <guruprasadkn1991@gmail.com>
2021-08-03 20:48:44 +0530

    replace string with constants

--- deliverysolution/src/test/java/com/everestengineering/DiscountCalculationTest.java
+++ deliverysolution/src/test/java/com/everestengineering/DiscountCalculationTest.java
@@ -5,6 +5,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.apache.log4j.Logger;
 import org.junit.jupiter.api.Test;
 
+import com.everestengineering.constant.DiscountConstant;
+import com.everestengineering.constant.ValidationConstant;
 import com.everestengineering.model.DiscountResponse;
 import com.everestengineering.service.DiscountService;
 
@@ -16,31 +18,33 @@ public class DiscountCalculationTest {
 	public void testDiscountCalculation() {
 		
 		DiscountResponse discountResponse = DiscountService.getInstance().
-				calculateDiscountByCouponCode("OFR001", 100, 5, 5);
+				calculateDiscountByCouponCode(DiscountConstant.CouponNames.OFR001.toString(), 100, 5, 5);
 		assertEquals(0.0, discountResponse.getTotalDiscountInAmount());
 		
 		discountResponse = DiscountService.getInstance().
-				calculateDiscountByCouponCode("OFR001", 100, 75, 5);
+				calculateDiscountByCouponCode(DiscountConstant.CouponNames.OFR001.toString(), 100, 75, 5);
 		assertEquals(10.0, discountResponse.getTotalDiscountInAmount());
 		
-		discountResponse = DiscountService.getInstance().calculateDiscountByCouponCode("OFR002", 1500,
-				15, 5);
+		discountResponse = DiscountService.getInstance().calculateDiscountByCouponCode(
+				DiscountConstant.CouponNames.OFR002.toString(), 1500, 15, 5);
 		assertEquals(0.0, discountResponse.getTotalDiscountInAmount());
 		
-		discountResponse = DiscountService.getInstance().calculateDiscountByCouponCode("OFR002", 1500,
-				135, 75);
+		discountResponse = DiscountService.getInstance().calculateDiscountByCouponCode(
+				DiscountConstant.CouponNames.OFR002.toString(), 1500, 135, 75);
 		assertEquals(105.0, discountResponse.getTotalDiscountInAmount());
 		
-		discountResponse = DiscountService.getInstance().calculateDiscountByCouponCode("OFR003", 700,
+		discountResponse = DiscountService.getInstance().calculateDiscountByCouponCode(
+				DiscountConstant.CouponNames.OFR003.toString(), 700,
 				10, 100);
 		assertEquals(35.0, discountResponse.getTotalDiscountInAmount());
 		
-		discountResponse = DiscountService.getInstance().calculateDiscountByCouponCode("OFR003", 700,
+		discountResponse = DiscountService.getInstance().calculateDiscountByCouponCode(
+				DiscountConstant.CouponNames.OFR003.toString(), 700,
 				5, 100);
 		assertEquals(0.0, discountResponse.getTotalDiscountInAmount());
 		
-		discountResponse = DiscountService.getInstance().calculateDiscountByCouponCode("OFR004", 700,
-				10, 100);
+		discountResponse = DiscountService.getInstance().calculateDiscountByCouponCode(
+				ValidationConstant.INVALID_INPUT, 700, 10, 100);
 		assertEquals(0.0, discountResponse.getTotalDiscountInAmount());
 	}
 }
